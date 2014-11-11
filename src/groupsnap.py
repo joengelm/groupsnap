90732d965efe998443713eb0a293a6cfbbdacd89
joengelm <joseph_engelman@brown.edu>
2014-11-10 22:46:13 -0500

    Initial commit

---
+++ src/groupsnap.py
@@ -0,0 +1,66 @@
+from snapchat import *
+from time import sleep
+import getpass
+import os
+import sys
+
+username = ""
+path = ""
+
+def run(password, post_count):
+	s = Snapchat()
+	s.login(username, password)
+	if not s.logged_in:
+		print "Invalid login credentials"
+		return
+	while True:
+		snaps = s.get_snaps()
+		for snap in snaps:
+			download(s, snap)
+		s.clear_feed()
+		for filename in os.listdir(path):
+			media_type = 0
+			extension = filename.split('.')[-1]
+			if extension == "mp4":
+				media_type = s.MEDIA_VIDEO
+			elif extension == "jpg" or extension == "jpeg":
+				media_type = s.MEDIA_IMAGE
+			else:
+				continue
+			media_id = s.upload(media_type, path + filename)
+			s.add_story(media_id, media_type, "Post #" + str(post_count), int(filename.split("+")[0]))
+			os.remove(path + filename)
+			post_count += 1
+		sleep(30)
+		s.login(username, password)
+	
+def download(s, snap):
+    """Download a specific snap"""
+
+    id = snap['id']
+    name = snap['sender']
+    time = str(snap['time'])
+
+    result = s.get_media(id)
+
+    if not result:
+        return False
+
+    ext = s.is_media(result)
+    filename = '{}+{}+{}.{}'.format(time, name, id, ext)
+    full_path = path + filename
+    with open(full_path, 'wb') as fout:
+        fout.write(result)
+    return True
+
+if __name__ == '__main__':
+	username = raw_input("Username: ")
+	path = "../" + username + "/"
+	password = getpass.getpass('Password: ')
+	post_count = 0
+	if len(sys.argv) > 1:
+		try:
+			post_count = int(sys.argv[1])
+		except ValueError:
+			post_count = 1
+	run(password, post_count)
