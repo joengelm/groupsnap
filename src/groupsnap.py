b4d002d6dc5314685bfcad5c46dbb752f717d7ca
joengelm <joseph_engelman@brown.edu>
2014-11-11 17:50:53 -0500

    Implemented total anonymity and added logging

--- src/groupsnap.py
+++ src/groupsnap.py
@@ -1,8 +1,7 @@
 from snapchat import *
 from time import sleep
-import getpass
-import os
-import sys
+import getpass, os, sys
+import logging
 
 username = ""
 path = ""
@@ -14,13 +13,18 @@ def run(password, init_post_count):
 	if not s.logged_in:
 		print "Invalid login credentials"
 		return
+	logging.basicConfig(filename='../gs.log', level=logging.DEBUG, format='%(asctime)s - ' + username + ': %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+	logging.info("initializing")
 	while True:
+		logging.info("fetching snaps")
 		snaps = s.get_snaps()
 		for snap in snaps:
+			logging.info("downloaded snap")
 			download(s, snap)
+		logging.info("clearing feed")
 		s.clear_feed()
 		for filename in os.listdir(path):
-			print "Processing: ", filename
+			logging.info("processing snap " + str(post_count))
 			media_type = 0
 			extension = filename.split('.')[-1]
 			if extension == "mp4":
@@ -29,32 +33,38 @@ def run(password, init_post_count):
 				media_type = s.MEDIA_IMAGE
 			else:
 				continue
+			logging.info("uploading snap " + str(post_count) + " as a " + extension)
 			media_id = s.upload(media_type, path + filename)
 			time = 10
 			try:
 				time = int(filename.split('+')[0])
 			except ValueError:
 				time = 10
+			logging.info("adding snap " + str(post_count) + " to story")
 			s.add_story(media_id, media_type, "Post #" + str(post_count), time)
+			logging.info("removing snap " + str(post_count) + " from directory")
 			os.remove(path + filename)
 			post_count += 1
+		logging.info("sleeping")
 		sleep(300)
+		logging.info("logging in")
 		s.login(username, password)
+		logging.info("logged in")
 	
 def download(s, snap):
     """Download a specific snap"""
 
-    id = snap['id']
+    snap_id = snap['id']
     name = snap['sender']
     time = str(snap['time'])
 
-    result = s.get_media(id)
+    result = s.get_media(snap_id)
 
     if not result:
         return False
 
     ext = s.is_media(result)
-    filename = '{}+{}+{}.{}'.format(time, name, id, ext)
+    filename = '{}+{}.{}'.format(time, snap_id, ext)
     full_path = path + filename
     with open(full_path, 'wb') as fout:
         fout.write(result)
